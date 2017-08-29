c41ffbaa79c13bf40ea5952bfaad15a2193dd453
Lasac <github123>
2017-08-29 17:25:53 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ let express = require('express');
 let bodyParser = require('body-parser');
 
 
+let {ObjectId} = require('mongodb');
+
+
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
@@ -30,6 +33,37 @@ app.get('/todos', function (req, res) {
     }, function (e) {
         res.status(400).send(e);
     });
+});
+
+
+// GET /todos/123123
+app.get('/todos/:id', function (req, res) {
+    let id = req.params.id;
+
+    // Validate ID
+    if (!ObjectId.isValid(id)) {
+        // 404 - send back empty send
+        return res.status(404).send('ID format is not valid.');
+    }
+
+    // find by id
+    Todo.findById(id).then(function (todo) {
+        // success
+
+        if (!todo) {
+            // if no todo - send back 404 with empty body
+            return res.status(404).send('Wrong ID');
+        }
+        // if todo - send it back
+        res.send({todo});
+
+    }).catch(function (e) {
+        // error
+        // 400 - and send empty body back
+        res.status(400).send(e);
+    });
+
+
 });
 
 
